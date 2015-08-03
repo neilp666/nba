c636b1feda4dd7c86c539bbd2cdabb54d609b952
neil patel <neil@myindianweddingapp.com>
2015-08-03 10:36:52 +0100

    Added NBA teams

--- config/initializers/nba.rb
+++ config/initializers/nba.rb
@@ -5,3 +5,5 @@ url = "https://en.wikipedia.org/wiki/List_of_current_NBA_team_rosters" #add to t
 page = Nokogiri::HTML(open(url))  #add url to page variable
 
 puts page
+
+puts page.css('li.toclevel-3')
